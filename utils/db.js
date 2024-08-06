a98fb9312befeb0f40c76f48b46c5dca1a16a173
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-08-06 06:12:18 +0100

    Task2

---
+++ utils/db.js
@@ -0,0 +1,39 @@
+import MongoClient from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const dbHOST = process.env.DB_HOST || 'localhost';
+    const dbPORT = process.env.DB_PORT || 27017;
+    this.db = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient(`mongodb://${dbHOST}:${dbPORT}`);
+  }
+
+  isAlive() {
+    return !!this.client && !!this.client.topology && this.client.topology.isConnected();
+    // return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    try {
+      const userCount = await this.client.db(this.db).collection('users').countDocuments();
+      return userCount;
+    } catch (e) {
+      console.error('Error counting documents', e);
+      return null;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      const fileCount = await this.client.db(this.db).collection('files').countDocuments();
+      return fileCount;
+    } catch (e) {
+      console.error('Error counting documents', e);
+      return null;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
